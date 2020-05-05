9a01d9fd799f454cab8e8f16cc0bde997affb1d7
kongtao <kongtao@guazi.com>
2020-05-05 19:54:03 +0800

    1

--- src/com/ktao/leetcode/搜索/组合/CombinationSum.java
+++ src/com/ktao/leetcode/搜索/组合/CombinationSum.java
@@ -40,11 +40,12 @@ public class CombinationSum {
      */
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        helper(res, 0, new ArrayList<>(), candidates, target);
+        if (candidates == null || candidates.length == 0) return res;
+        helper(res, new ArrayList<>(), candidates, target, 0);
         return res;
     }
 
-    private void helper(List<List<Integer>> res, int start, List<Integer> list, int[] candidates, int target){
+    private void helper(List<List<Integer>> res, List<Integer> list, int[] candidates, int target, int start){
         if (target < 0) return;
         if (target == 0){
             res.add(new ArrayList<>(list));
@@ -52,10 +53,8 @@ public class CombinationSum {
         }
         for (int i = start; i < candidates.length; i++){
             list.add(candidates[i]);
-            helper(res, start ,list, candidates, target - candidates[i]);
+            helper(res, list, candidates, target - candidates[i], i);
             list.remove(list.size() - 1);
-            // ！！！star+1
-            start +=1;
         }
     }
 }
